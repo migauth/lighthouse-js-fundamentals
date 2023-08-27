91cdf8a0c4cfbd6e632bf71b2b212aa7ee9fe750
Michael Gauthier <mikegauthier222@hotmail.com>
2023-08-27 10:00:33 -0700

    add new files

---
+++ odd.js
@@ -0,0 +1,12 @@
+const isOdd = function(num) {
+  if (num % 2 === 0){
+    console.log('number is even');
+  } else {
+    console.log('number is odd');
+  }
+}
+
+isOdd(10);
+isOdd(11);
+isOdd(55);
+isOdd(100);
--- practise.js
+++ practise.js
@@ -1,5 +1,13 @@
-const returnSayHello = function (name) {
-  return "Hello, " + name;
-};
-const greeting = returnSayHello("John");
-console.log(returnSayHello("joh"));
+function concat(arr1, arr2) {
+  let newArr = arr1;
+  for (let i = 0; i < arr2.length; i++) {
+    const element = arr2[i];
+    arr1.push(element);
+  }
+  return newArr;
+}
+
+console.log(concat([1, 2, 3], [4, 5, 6]));
+console.log(concat([0, 3, 1], [9, 7, 2]), "=?", [0, 3, 1, 9, 7, 2]);
+console.log(concat([], [9, 7, 2]), "=?", [9, 7, 2]);
+console.log(concat([5, 10], []), "=?", [5, 10]);
